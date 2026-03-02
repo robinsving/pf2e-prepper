13a7aa1d0cfe83b1575388b0a2601bf4fd715832
Robin Sving <robin.sving@kambi.com>
2026-03-02 22:28:01 +0100

    test: fix the mocks

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,45 +1,26 @@
-// Mock the Application class
-global.Application = class {
-    constructor() {
-        this.options = {};
-    }
-    render(force = false) {
-        console.log("Mock render called with force:", force);
-    }
-    static get defaultOptions() {
-        return {};
-    }
-};
 
 // Mock the global game object
 global.game = {
     settings: {
-        get: vi.fn((module, setting) => {
+        get: vi.fn((_, setting) => {
             if (setting === "showStatusIcons") return true; // Enable status icons
             return false;
         }),
     },
-    modules: {
-        get: vi.fn(() => ({
-            api: { },
-        })),
-    },
-    combats: [
-        {
-            active: true,
-            round: 3,
-        },
-    ],
-    user: {
-        isGM: true,
-    },
+    i18n: {
+        localize: vi.fn((key) => {
+            const translations = {
+                'PREPPER.Title': 'Prepper',
+            };
+            return translations[key] || key;
+        }),
+    }
 };
 
 // Mock the Hooks object
 global.Hooks = {
     on: vi.fn(),
     once: vi.fn(),
-    callAll: vi.fn(),
 };
 
 // Mock the foundry object
@@ -47,16 +28,26 @@ global.foundry = {
     utils: {
         randomID: vi.fn(() => `id_${Math.random().toString(36).substring(2, 9)}`), // Generate a random ID
     },
-};
+    applications: {
+        api: {
+            ApplicationV2: class {
+                constructor() {
+                }
+            },
+            HandlebarsApplicationMixin: (Base) =>
+                class extends Base {
+                    constructor(...args) {
+                        super(...args)
+                    }
 
-/*
-// Mock the Handlebars object
-global.Handlebars = {
-    registerHelper: vi.fn((name, fn) => {
-        console.log(`Mock Handlebars helper registered: ${name}`);
-    }),
-    compile: vi.fn((template) => {
-        console.log("Mock Handlebars template compiled");
-        return (context) => `Mock template output for context: ${JSON.stringify(context)}`;
-    }),
-};*/
+                    render() {
+                        return true
+                    }
+
+                    close() {
+                        return true
+                    }
+                }
+        }
+    }
+};
